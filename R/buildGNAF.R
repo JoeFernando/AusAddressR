fa197872bc89d00485c5b6b5035ff3c889434048
JohnBuswell <john.buswell@uqconnect.edu.au>
2016-06-23 22:40:31 +1000

    Fleshing out building DB

--- R/buildGNAF.R
+++ R/buildGNAF.R
@@ -9,8 +9,26 @@
 #'
 #' @examples
 #' buildGNAF('~/GNAF')
-buildGNAF <- function(path=NULL){
+buildGNAF <- function(gnafPath, writePath=NULL){
   require(RSQLite)
-  message("* Not doing anything yet")
+  tryCatch(
+    {
+      m = simpleMessage('Unzipped')
+      if(grep('\\.zip$',gnafPath)){
+        unzip(gnafPath,exdir=writePath)
+        writePathDir = paste0(writePath,'/GNAF')
+        if(!dir.exists(writePathDir)){
+          dir.create(writePathDir)
+        }
+        unzip(gnafPath,exdir=writePathDir)
+      }
+    }
+  ,error = simpleError('Failed to unzip')
+  ,finally = function(m) m
+  )
+  db = dbConnect(SQLite(),'GNAF.db')
+
+
+
   return(TRUE)
 }
